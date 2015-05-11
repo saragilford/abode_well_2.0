913eaafcb8f709987e476273889ff1f739c78931
Ginny Martin & Brandi M. Ford <pair+gwmartin.lilpaw@devbootcamp.com>
2015-05-11 10:59:26 -0700

    add views from version 1.0 and js beginnings

---
+++ app/assets/javascripts/maintissue.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+$('.maint-issue-form').on('submit', function(event){
+    event.preventDefault();
+    debugger
+    request = $.ajax({
+      url: 'http://localhost:3000/report',
+      method: "post",
+      data: $(this).serialize(),
+      dataType: 'json'
+    });
+    request.done(function(data){
+       console.log(data)
+       // where do we send this data?
+
+
+    });
+  });
+
+
+});
+
+
