5618e6504d68ce760924fe3c2ac9be92ce1be237
Ginny Martin & Brandi M. Ford <pair+gwmartin.lilpaw@devbootcamp.com>
2015-05-11 12:13:24 -0700

    able to view profile page without ajax

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -14,12 +14,34 @@ class BuildingsController < ApplicationController
   end
 
   def show
+    @report_categories = ["RentIncrease", "MaintenenceIssue", "EvictionNotice", "OtherHarassment"]
+    @letter_options = ["Ellis Act", "Landlord Move-In", "Condo Conversion"]
+    @building = Building.where(id: params[:id]).first
 
-    # @building=Building.where(address:params[:address])
-    @building=Building.first
-    @score=@building.badge_score
+    @reports_array = []
+    @building.harassments.each do |report|
+      @reports_array << report
+    end
 
-    render json: [@building, theScoopsScore:@score]
+    @building.fix_orders.each do |report|
+      @reports_array << report
+    end
+
+    @building.eviction_notices.each do |report|
+      @reports_array << report
+    end
+
+    @building.rent_notices.each do |report|
+      @reports_array << report
+    end
+
+  @reports_array.sort!{|a,b|a.updated_at <=> b.updated_at}
+
+  @reports_array = @reports_array.first(10)
+
+
+
+    render :"buildings/building_profile"
     # render json:  @building
   end
 
