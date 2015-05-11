8139be3574e77d610811a979735f502c7f43692f
Sara Gilford <sara.gilford@gmail.com>
2015-05-11 10:43:32 -0700

    Add rspec

---
+++ spec/models/building_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Building, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 
+
+
+end
---
+++ spec/models/scoopsSpec.rb
@@ -0,0 +1,17 @@
+Algorithm
+
+buildings model
+
+it "returns all related EllisActs for a specific building"
+	@building.ellis_acts
+
+	@ellis =@build.ellis_acts.build(filing_number:"3343-43434")
+
+
+describe do 
+
+	it ""do
+		expect
+	end
+	
+end
