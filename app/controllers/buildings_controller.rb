be7ec18e344af88ce5b83a6f581d70b58821f1b1
NeuTrix <NeuTrix@brandgeneering.com>
2015-05-12 10:55:02 -0700

    comment out duplicate search function in bulding controller

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -4,16 +4,16 @@ class BuildingsController < ApplicationController
     render :"buildings/index"
   end
 
-  def search
-    @only_zips = []
-    @buildings = Building.where(zip_code: params[:zip_code])
-      @buildings.each do |building|
-        if building.address.only_numbers == params[:address].only_numbers
-          @only_zips << building
-        end
-      end
-    return @only_zips
-  end
+  # def search
+  #   @only_zips = []
+  #   @buildings = Building.where(zip_code: params[:zip_code])
+  #     @buildings.each do |building|
+  #       if building.address.only_numbers == params[:address].only_numbers
+  #         @only_zips << building
+  #       end
+  #     end
+  #   return @only_zips
+  # end
 
   def search
     @only_zips = []
