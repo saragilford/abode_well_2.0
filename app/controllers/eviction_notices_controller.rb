47775cfd040984da63c8a365c20fab6a76f5a436
Sara Gilford <sara.gilford@gmail.com>
2015-06-11 12:29:55 -0700

    Fix eviction form functionality

--- app/controllers/eviction_notices_controller.rb
+++ app/controllers/eviction_notices_controller.rb
@@ -1,13 +1,25 @@
 class EvictionNoticesController < ApplicationController
 
   def create
-    @building = Building.where(id: params[:building_id]).first ||
+    @building = Building.where(id: params[:building_id]).first
     @evict = EvictionNotice.new(category: params[:category],comment: params[:comment],building_id: @building.id)
+
+    p '*' * 80
+    p params
+    p '&' * 80
+    p @building
+    p '^' * 80
+    p @evict
+    p '$' * 80
     if @evict.save
       respond_to do |format|
         format.json { render json: @evict.to_json }
         format.html {redirect_to building_path(@building)}
       end
+    else
+      p "%" * 80
+      p "NOT SAVED"
+      p "%" * 80
     end
 
   end
