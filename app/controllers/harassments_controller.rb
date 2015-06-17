809e75da507001ab1b69309216cc71d2405400b5
gwmartin <ginny.w.martin@gmail.com>
2015-06-17 17:47:22 -0500

    map rendering now controller broken at the moment. trying to figure out how to merge report name with the json object

--- app/controllers/harassments_controller.rb
+++ app/controllers/harassments_controller.rb
@@ -3,9 +3,12 @@ class HarassmentsController < ApplicationController
   def create
     @building = Building.where(id: params[:building_id]).first
     @harass = Harassment.new(category: params[:category],comment: params[:comment],building_id: @building.id)
+    harass_hash = @harass.to_h
+    report_type = {class: "Harassment Report")
+
     if @harass.save
       respond_to do |format|
-        format.json { render json: @harass.to_json }
+        format.json { render json: (report_type + harass_hash).to_json }
         format.html {redirect_to building_path(@building)}
       end
     end
