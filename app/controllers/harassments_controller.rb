fe141578c21977d13fadd0c7fdbd784dc01a989b
gwmartin <ginny.w.martin@gmail.com>
2015-06-18 14:52:38 -0500

    change hash to .merge and fixed error. error msg is 404

--- app/controllers/harassments_controller.rb
+++ app/controllers/harassments_controller.rb
@@ -4,12 +4,12 @@ class HarassmentsController < ApplicationController
     @building = Building.where(id: params[:building_id]).first
     @harass = Harassment.new(category: params[:category],comment: params[:comment],building_id: @building.id)
     harass_hash = @harass.to_h
-    report_type = {class: "Harassment Report")
+    report_type = {class: "Harassment Report"}
+
 
     if @harass.save
       respond_to do |format|
-        format.json { render json: (report_type + harass_hash).to_json }
-        format.html {redirect_to building_path(@building)}
+        render json: report_type.merge(harass_hash).to_json
       end
     end
   end
