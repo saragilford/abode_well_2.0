1b84af0e8e3f1f9ac21d751e48e14a4982ae1480
Sara Gilford <sara.gilford@gmail.com>
2015-06-18 15:24:37 -0700

    Fix harassment controller

--- app/controllers/harassments_controller.rb
+++ app/controllers/harassments_controller.rb
@@ -3,13 +3,13 @@ class HarassmentsController < ApplicationController
   def create
     @building = Building.where(id: params[:building_id]).first
     @harass = Harassment.new(category: params[:category],comment: params[:comment],building_id: @building.id)
-    harass_hash = @harass.to_h
+    harass_hash = @harass.attributes
     report_type = {class: "Harassment Report"}
 
 
     if @harass.save
       # respond_to do |format|
-        render json: @harass.to_json
+        render json: harass_hash.merge(report_type).to_json
         # report_type.merge(harass_hash).to_json
       # end
     end
